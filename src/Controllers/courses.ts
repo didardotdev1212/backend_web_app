b07ed1835aadeadca4502a70fd29b758155bfe95
Didar Abdulkhaliq <me@didar.dev>
2025-09-29 16:32:52 +0300

    Refactor GetCourseByID to include category name in course retrieval

--- src/Controllers/courses.ts
+++ src/Controllers/courses.ts
@@ -4,13 +4,16 @@ import db from "../lib/knex";
 const GetCourseByID = async (req: any, res: Response) => {
   const { id } = req.params;
   try {
-    const courses = await db("courses").select("*").where({ id: id }).first();
+    const courses = await db("courses")
+      .select("courses.*", "categories.name as category_name")
+      .where("courses.id", id)
+      .leftJoin("categories", "courses.category_id", "categories.id")
+      .first();
     if (!courses) {
       return res
         .status(404)
         .json({ success: false, message: "Course not found" });
     }
-
     res.status(200).json({ success: true, data: courses });
   } catch (error) {
     console.error("Error fetching courses:", error);
