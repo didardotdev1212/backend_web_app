ebf2e95ff27cc2cebaf25a618a59039382e5505d
Didar Abdulkhaliq <me@didar.dev>
2025-10-08 16:07:09 +0300

    Implement course deletion functionality and enhance course creation to handle lesson uploads

--- src/Controllers/Admin/courses.ts
+++ src/Controllers/Admin/courses.ts
@@ -15,18 +15,40 @@ const GetCourses = async (req: any, res: Response) => {
 
 const CreateCourse = async (req: any, res: Response) => {
   try {
-    const { title, description, category_id, price } = req.body;
-    const image_url = req.file ? req.file.path : "";
+    let { title, description, category_id, price, lessons } = req.body;
+    console.log(req.file);
     const user_id = req.user.id;
-    await db("courses").insert({
-      title: title,
-      description: description,
-      category_id: category_id,
-      image: image_url,
-      price: price,
-      created_by: user_id,
-      created_at: new Date(),
-    });
+    const imageurl = req.file
+      ? `https://pub-8b2d28096f434a889120e98b6606a84e.r2.dev/${req?.file?.key}`
+      : "";
+    const [insertedId] = await db("courses")
+      .insert({
+        title: title,
+        description: description,
+        category_id: category_id,
+        image: imageurl,
+        price: price,
+        created_by: user_id,
+        created_at: new Date(),
+      })
+      .returning("id"); // Specify the name of your ID column
+
+    try {
+      lessons = JSON.parse(lessons);
+    } catch (error) {
+      lessons = [];
+    }
+
+    if (lessons && lessons.length > 0) {
+      const lessonsToInsert = lessons?.map((lesson: any, index: number) => ({
+        course_id: insertedId?.id || insertedId, // Adjust based on your DB returning behavior
+        name: lesson.name,
+        video_url: lesson.video_url,
+        order: index + 1,
+      }));
+      await db("lessons").insert(lessonsToInsert);
+    }
+
     res
       .status(201)
       .json({ success: true, message: "Course created successfully" });
@@ -36,4 +58,36 @@ const CreateCourse = async (req: any, res: Response) => {
   }
 };
 
-export { CreateCourse, GetCourses };
+const DeleteCourse = async (req: any, res: Response) => {
+  try {
+    const { courseId } = req.params;
+    const user_id = req.user.id;
+    const isExists = await db("courses")
+      .select("*")
+      .where({ id: courseId })
+      .first();
+    if (!isExists) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    /// is created by the same user
+    if (isExists.created_by !== user_id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this course" });
+    }
+    await db("courses").where({ id: courseId }).del();
+    /// delete lessons
+    await db("lessons").where({ course_id: courseId }).del();
+    //// delete enrollments
+    await db("EnrolledCourses").where({ course_id: courseId }).del();
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Course deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting course:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export { CreateCourse, GetCourses, DeleteCourse };
--- src/Routes/Admin/courses.ts
+++ src/Routes/Admin/courses.ts
@@ -3,11 +3,16 @@ import { Router } from "express";
 import upload from "../../Middleware/Upload";
 import AdminAuth from "../../Middleware//Admin/AdminAuth";
 //// controllers
-import { CreateCourse, GetCourses } from "../../Controllers/Admin/courses";
+import {
+  CreateCourse,
+  GetCourses,
+  DeleteCourse,
+} from "../../Controllers/Admin/courses";
 
 const router = Router();
 
 router.get("/", AdminAuth, GetCourses);
 router.post("/create", AdminAuth, upload.single("image"), CreateCourse);
+router.delete("/delete/:courseId", AdminAuth, DeleteCourse);
 
 export default router;
